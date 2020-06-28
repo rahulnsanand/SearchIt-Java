c63c96d5770c16fae0de25104462a44e7e6b1023
theflopguy <theflopguymail@gmail.com>
2020-06-28 22:55:00 +0530

    Get Restaurant ID From Source Code

--- src/main/java/com/searchit/tfg/TESTING/RestaurantWebSearch.java
+++ src/main/java/com/searchit/tfg/TESTING/RestaurantWebSearch.java
@@ -1,5 +1,46 @@
 package com.searchit.tfg.TESTING;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.DataNode;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+
+import java.io.IOException;
+
 public class RestaurantWebSearch {
 
+    public void GetID(String URL){
+        Document doc = null;
+
+        try {
+            doc = Jsoup.connect(URL).get();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        assert doc != null;
+        Elements scriptElements = doc.getElementsByTag("script");
+
+        for (Element element :scriptElements ){
+            for (DataNode node : element.dataNodes()) {
+                if(node.toString().contains("res_id")){
+                    System.out.println("Got Json");
+                    String data = node.getWholeData();
+                    String[] split = data.split("\n");
+                    for(String i :split){
+                        if(i.contains("res_id")) {
+                            String[] JsonData = i.split(":");
+                            for(int j=0; j<JsonData.length;j++){
+                                if(JsonData[j].contains("res_id")){
+                                    String[] res_idWhole = JsonData[j+1].split(",");
+                                    System.out.println(res_idWhole[0]);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
